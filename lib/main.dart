0886d3839af09fb5e0f3e6413ef038c02389c9ff
Riadh-HAJJI <hajjriadh@gmail.com>
2025-10-31 09:25:59 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyWidget());
+}
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+//essaieprojet-79165-default-rtdb.europe-west1.firebasedatabase.app/
